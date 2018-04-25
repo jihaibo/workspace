fd96ffffbdcba2477d535366ab33c375afd73c1d
jihaibo <1038726264@qq.com>
2018-04-25 19:05:13 +0800

    封装测试

---
+++ meione_test/mei1_unittest/project/lianxi/baidu_search.py
@@ -0,0 +1,31 @@
+# -*- coding:utf-8 -*-
+
+import time
+import sys
+from selenium import webdriver
+sys.path.append('/path/to/project/')
+from basemanage.basepage import BasePage
+
+
+class BaiduSearch(object):
+
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.implicitly_wait(10)
+
+    basepage = BasePage(driver)
+
+    def open_baidu(self):
+        self.basepage.open_url("https://www.baidu.com")
+        time.sleep(1)
+
+    def test_search(self):
+        self.driver.find_element_by_id('kw').send_keys("Selenium")
+        time.sleep(1)
+        self.basepage.back()
+        self.basepage.forward()
+        self.basepage.quit_browser()
+
+baidu = BaiduSearch()
+baidu.open_baidu()
+baidu.test_search()
